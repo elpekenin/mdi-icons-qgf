5721225643bbfc795ede5d98e0136ea72b17d2c2
elpekenin <elpekenin@elpekenin.dev>
2023-03-01 12:38:58 +0100

    Generalize scripts

--- png2qgf.bash
+++ png2qgf.bash
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+INPUT_PATH=./templarian-png
+OUTPUT_PATH=../templarian-qgf # relative to input path
+TARGET_FMT=mono2
+
+# From <https://unix.stackexchange.com/questions/103920/parallelize-a-bash-for-loop>
+
+# Number of concurrent tasks. Beware, a big one could crash your computer
 N=32
 (
-cd ./templarian-png || exit
+cd $INPUT_PATH || exit
 for file in ./*; do 
    ((i=i%N)); ((i++==0)) && wait
-   qmk painter-convert-graphics -f mono2 -i $file -o ../templarian-qgf &
+   qmk painter-convert-graphics -f $TARGET_FMT -i $file -o $OUTPUT_PATH &
 done
 )
--- svg2png.bash
+++ svg2png.bash
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+INPUT_PATH=./templarian-svg
+OUTPUT_PATH=../templarian-png # relative to input path
+IMG_SIZE=48
+
 # From <https://unix.stackexchange.com/questions/103920/parallelize-a-bash-for-loop>
 
 # Number of concurrent tasks. Beware, a big one could crash your computer
 N=32
 (
-cd ./templarian-svg || exit
+cd $INPUT_PATH || exit
 for file in ./*; do 
    ((i=i%N)); ((i++==0)) && wait
-#                                                    ↓ this is the output image's size
-   svgexport $file ../templarian-png/${file/svg/png} 48: "svg{fill: white;}" &
+#                                     ↓ same name as with png extension his is the output image's size
+   svgexport $file $OUTPUT_PATH/${file/.svg/.png} $IMG_SIZE: "svg{fill: white;}" &
 #                                                                    ↑ need to make the SVG white so it works
 done 
 )
